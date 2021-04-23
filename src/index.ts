5f7b9e78f0d3e3ba42455e8c26e7e3b0188fb800
James Zhang <liulfbaby@gmail.com>
2021-04-24 00:57:13 +0800

    feat: add koa-router

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx: any) => {
-  if (ctx.url === '/user') {
-    if (ctx.method === 'GET') {
-      ctx.body = 'get to user!';
-    } else {
-      ctx.body = 'post to user!';
-    }
-  } else if (ctx.url.match(/\/users\/\w+/)) {
-    const userId = ctx.url.match(/\/users\/(\w+)/)[1];
-    ctx.body = `this is user ${userId}`;
-  } else ctx.body = 'Hello';
+router.get('/', async (ctx: any) => {
+  ctx.body = 'index page';
 });
 
+app.use(router.routes());
 app.listen(3003);

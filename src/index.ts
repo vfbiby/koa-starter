7fe4fb970d5f63f98ab36cd056c4453ff8eb59a1
James Zhang <liulfbaby@gmail.com>
2021-04-24 01:03:02 +0800

    feat: get post and url params by koa-router

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,20 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
+const usersRouter = new Router({ prefix: '/users' });
 
 router.get('/', async (ctx: any) => {
   ctx.body = 'index page';
 });
 
+usersRouter.get('/', async (ctx: any) => {
+  ctx.body = 'this user list';
+});
+
+usersRouter.get('/:id', async (ctx: any) => {
+  ctx.body = `this is user ${ctx.params.id}`;
+});
+
 app.use(router.routes());
+app.use(usersRouter.routes());
 app.listen(3003);

679d839fc48574fe8a30533f9c3ed3ef3d5ea436
James Zhang <liulfbaby@gmail.com>
2021-04-24 00:52:27 +0800

    feat: implement route by handmake

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use((ctx:any) => {
-  ctx.body = 'Hello';
+app.use(async (ctx: any) => {
+  if (ctx.url === '/user') {
+    if (ctx.method === 'GET') {
+      ctx.body = 'get to user!';
+    } else {
+      ctx.body = 'post to user!';
+    }
+  } else if (ctx.url.match(/\/users\/\w+/)) {
+    const userId = ctx.url.match(/\/users\/(\w+)/)[1];
+    ctx.body = `this is user ${userId}`;
+  } else ctx.body = 'Hello';
 });
 
 app.listen(3003);

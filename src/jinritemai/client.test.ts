6bcb2f2e27620c570821ab8a19a2fe9e63d22ae4
James Zhang <liulfbaby@gmail.com>
2021-05-24 01:19:22 +0800

    fix: get appKey from env file in test

--- src/jinritemai/client.test.ts
+++ src/jinritemai/client.test.ts
@@ -1,23 +1,45 @@
 import { JrtmClient } from './client';
 import { dateToSecond } from './utils';
 
-const appKey = '6959524271349777951';
-const appSecret = 'cdc0f953-fa9c-4fb0-a4eb-36308291b6ea';
-const accessToken = 'b9e8e8e3-6654-450d-8c1e-9234da785141';
-const client = new JrtmClient(appKey, appSecret, accessToken);
-
 describe('client', () => {
-  it('searchOrders', async () => {
-    const data = await client.searchOrders({
-      page: 0,
-      size: 10,
-      create_time_start: dateToSecond(new Date('2021-05-10')),
-    });
-    //console.log(JSON.stringify(data));
-    data.should.include.keys('data');
-    data.data.shop_order_list.should.have.lengthOf.above(0);
+  let appKey: string, appSecret: string, accessToken: string;
 
-    expect(data.err_no).to.eq(0);
-    expect(data.message).to.eq('success');
+  beforeEach(() => {
+    appKey = process.env.APPKEY ? process.env.APPKEY : '';
+    appSecret = process.env.APPSECRET ? process.env.APPSECRET : '';
+    accessToken = process.env.ACCESSTOKEN ? process.env.ACCESSTOKEN : '';
+  });
+
+  describe('success', () => {
+    it('should searchOrders with 3 params at least', async () => {
+      const client = new JrtmClient(appKey, appSecret, accessToken);
+      const data = await client.searchOrders({
+        page: 0,
+        size: 10,
+        create_time_start: dateToSecond(new Date('2021-05-10')),
+      });
+      //console.log(JSON.stringify(data));
+      data.should.include.keys('data');
+      data.data.shop_order_list.should.have.lengthOf.above(0);
+      data.err_no.should.to.eq(0);
+      data.message.should.to.eq('success');
+    });
+  });
+
+  describe('failure', () => {
+    it('should throw error while app_key is not valid', async () => {
+      const inValidAppKey = '6959524271349777952';
+      appKey = inValidAppKey;
+      const client = new JrtmClient(inValidAppKey, appSecret, accessToken);
+      const data = await client.searchOrders({
+        page: 0,
+        size: 10,
+        create_time_start: dateToSecond(new Date('2021-05-10')),
+      });
+      //console.log(JSON.stringify(data));
+      data.should.not.include.keys('data');
+      data.err_no.should.to.eq(30001);
+      data.message.should.to.eq('认证失败，app_key不存在');
+    });
   });
 });

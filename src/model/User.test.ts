7a789215dabb0c0fb71d5f73f2c9fac0a36974f8
James Zhang <liulfbaby@gmail.com>
2021-05-27 00:23:20 +0800

    feat: clearDatabase before each test

--- src/model/User.test.ts
+++ src/model/User.test.ts
@@ -2,11 +2,12 @@ import mongoose from 'mongoose';
 const User = require('./User');
 
 describe('User Model', () => {
-    beforeEach(() => {
-        mongoose.connect('mongodb://localhost:27017/zhubo', {
+    beforeEach(async () => {
+        await mongoose.connect('mongodb://localhost:27017/zhubo', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+        clearDatabase();
     });
 
     it('should return empty array when user collection is not filled', async () => {
@@ -14,13 +15,19 @@ describe('User Model', () => {
         users.should.deep.equal([]);
     });
 
-    it('should store data', async () => {
+    it('should store data which fields on the model', async () => {
         await new User({ name: 'vfbiby' }).save();
         const user = await User.find({ name: 'vfbiby' });
         user.toString().should.include('vfbiby');
     });
 
-    afterEach(() => {
-        mongoose.connection.db.dropCollection('users');
+    it('should ingore column when saving the data', async () => {
+        await new User({ name: 'vfbiby', propertyNotOnTheModel: 20 }).save();
+        const user = await User.find({ name: 'vfbiby' });
+        user.toString().should.not.include(20);
     });
 });
+
+function clearDatabase(): void {
+    mongoose.connection.db.dropCollection('users');
+}
